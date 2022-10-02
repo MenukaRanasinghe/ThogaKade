ab8e4ffbb9b2a38d5cc7d0bdac8947025a554822
Menuka <hgmranasinghe@gmail.com>
2022-10-02 14:57:56 +0530

    1

---
+++ src/com/seekerscloud/Pos/controller/CustomerFormController.java
@@ -0,0 +1,4 @@
+package com.seekerscloud.Pos.controller;
+
+public class CustomerFormController {
+}
---
+++ src/com/seekerscloud/Pos/controller/DashboardFormController.java
@@ -0,0 +1,58 @@
+package com.seekerscloud.Pos.controller;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DashboardFormController {
+    public AnchorPane dashboardContext;
+    public Label lblDate;
+    public Label lblTime;
+
+    public void initialize(){
+        setDateAndTime();
+    }
+
+    private void setDateAndTime(){
+        //settime
+        Timeline time=new Timeline(
+                new KeyFrame(Duration.ZERO,e->{
+                    DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    lblTime.setText(LocalDateTime.now().format(formatter));
+                }),new KeyFrame(Duration.seconds(1))
+        );
+        time.setCycleCount(Animation.INDEFINITE);
+        time.play();
+    }
+
+    public void openCustomerFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("CustomerForm");
+    }
+
+    public void openItemFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openOrderDetailsOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openPlaceOrderFormOnAction(ActionEvent actionEvent) {
+    }
+
+    private void setUI(String UI) throws IOException {
+        Stage stage =(Stage) dashboardContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/"+UI+".fxml"))));
+        stage.centerOnScreen();
+    }
+}
