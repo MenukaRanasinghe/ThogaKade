cdacfde6284cd4b5a955715f6909a93f1ffcb2c4
Menuka <hgmranasinghe@gmail.com>
2022-10-02 15:12:07 +0530

    2

--- src/com/seekerscloud/Pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/Pos/controller/CustomerFormController.java
@@ -1,4 +1,14 @@
 package com.seekerscloud.Pos.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class CustomerFormController {
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtSalary;
+
+    public void saveCustomerOnAction(ActionEvent actionEvent) {
+    }
 }
