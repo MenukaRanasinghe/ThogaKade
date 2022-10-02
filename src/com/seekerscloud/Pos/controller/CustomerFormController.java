c78918d8b0d9aa67eae724fb20e429b59ef21558
Menuka <hgmranasinghe@gmail.com>
2022-10-02 22:33:33 +0530

    3

--- src/com/seekerscloud/Pos/controller/CustomerFormController.java
+++ src/com/seekerscloud/Pos/controller/CustomerFormController.java
@@ -1,7 +1,10 @@
 package com.seekerscloud.Pos.controller;
 
 import com.jfoenix.controls.JFXTextField;
+import com.seekerscloud.Pos.db.Database;
+import com.seekerscloud.Pos.modal.Customer;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 
 public class CustomerFormController {
     public JFXTextField txtID;
@@ -10,5 +13,13 @@ public class CustomerFormController {
     public JFXTextField txtSalary;
 
     public void saveCustomerOnAction(ActionEvent actionEvent) {
+        Customer c1=new Customer(txtID.getText(),txtName.getText(),txtAddress.getText(),Double.parseDouble(txtSalary.getText()));
+        boolean isSaved=Database.customerTable.add(c1);
+        if (isSaved){
+            new Alert(Alert.AlertType.INFORMATION,"Customer Saved !").show();
+        }
+        else {
+            new Alert(Alert.AlertType.WARNING,"Try Again !").show();
+        }
     }
 }
