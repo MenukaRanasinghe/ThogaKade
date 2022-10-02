8535abc50829fc499cbdffcef31cf317bda4606f
Menuka <hgmranasinghe@gmail.com>
2022-10-02 22:33:59 +0530

    3

---
+++ src/com/seekerscloud/Pos/db/Database.java
@@ -0,0 +1,10 @@
+package com.seekerscloud.Pos.db;
+
+import com.seekerscloud.Pos.modal.Customer;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<Customer> customerTable=new ArrayList<Customer>();
+
+}
---
+++ src/com/seekerscloud/Pos/modal/Customer.java
@@ -0,0 +1,48 @@
+package com.seekerscloud.Pos.modal;
+
+public class Customer {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+
+    public Customer(String id, String name, String address, double salary) {
+        this.setId(id);
+        this.setName(name);
+        this.setAddress(address);
+        this.setSalary(salary);
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
