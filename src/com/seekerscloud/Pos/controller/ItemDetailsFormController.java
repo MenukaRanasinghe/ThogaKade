c8c4f789ff40732c63ce0f4d42f6f4c496336f2b
Menuka <hgmranasinghe@gmail.com>
2022-10-20 17:50:41 +0530

    15

---
+++ src/com/seekerscloud/Pos/controller/ItemDetailsFormController.java
@@ -0,0 +1,26 @@
+package com.seekerscloud.Pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ItemDetailsFormController {
+    public AnchorPane itemDetailsContext;
+    public TableView tblItems;
+    public TableColumn colCode;
+    public TableColumn colUnitPrice;
+    public TableColumn colQty;
+    public TableColumn colTotal;
+
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        itemDetailsContext.getScene().getWindow();
+        Stage stage=(Stage) itemDetailsContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/DashboardForm.fxml"))));
+    }
+}
